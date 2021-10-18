180cfae41a8aa0c9473341940b389684cb2b57a6
AsaduzzamanSam <samrat.asad@gmail.com>
2021-10-18 17:21:50 +0600

    easy pass gen code

--- pass.py
+++ pass.py
@@ -15,3 +15,23 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+for l in range(0, nr_letters):
+    password.append(letters[random.randrange(0, len(letters))])
+
+for s in range(0, nr_symbols):
+    password.append(symbols[random.randrange(0, len(symbols))])
+
+for n in range(0, nr_numbers):
+    password.append(numbers[random.randrange(0, len(numbers))])
+
+easy_final_password = ""
+
+for itm in password:
+    easy_final_password += itm
+
+print(f"Easy one : {easy_final_password}")
